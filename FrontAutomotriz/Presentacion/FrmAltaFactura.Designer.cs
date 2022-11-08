35b6595d504a607708c39ab5c8d8dc9f00888159
JulianM980 <2martinezjulian@gmail.com>
2022-11-08 20:50:17 -0300

    FrmAltaFactura front terminado

--- FrontAutomotriz/Presentacion/FrmAltaFactura.Designer.cs
+++ FrontAutomotriz/Presentacion/FrmAltaFactura.Designer.cs
@@ -66,50 +66,63 @@
             // 
             // btnAceptar
             // 
+            this.btnAceptar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAceptar.AutoSize = true;
+            this.btnAceptar.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAceptar.Font = new System.Drawing.Font("Nirmala UI Semilight", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnAceptar.Location = new System.Drawing.Point(705, 576);
             this.btnAceptar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnAceptar.Name = "btnAceptar";
-            this.btnAceptar.Size = new System.Drawing.Size(88, 27);
+            this.btnAceptar.Size = new System.Drawing.Size(88, 31);
             this.btnAceptar.TabIndex = 52;
             this.btnAceptar.Text = "Aceptar";
-            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.UseVisualStyleBackColor = false;
             this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
             // 
             // btnCancelar
             // 
+            this.btnCancelar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancelar.AutoSize = true;
+            this.btnCancelar.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Nirmala UI Semilight", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnCancelar.Location = new System.Drawing.Point(584, 576);
             this.btnCancelar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(88, 27);
+            this.btnCancelar.Size = new System.Drawing.Size(88, 31);
             this.btnCancelar.TabIndex = 51;
             this.btnCancelar.Text = "Cancelar";
-            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.UseVisualStyleBackColor = false;
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // label9
             // 
+            this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label9.Location = new System.Drawing.Point(590, 531);
             this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(52, 17);
+            this.label9.Size = new System.Drawing.Size(55, 16);
             this.label9.TabIndex = 50;
             this.label9.Text = "Total $";
             // 
             // label8
             // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label8.Location = new System.Drawing.Point(561, 500);
             this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(77, 17);
+            this.label8.Size = new System.Drawing.Size(82, 16);
             this.label8.TabIndex = 49;
             this.label8.Text = "SubTotal $";
             // 
             // txtTotal
             // 
+            this.txtTotal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.txtTotal.Enabled = false;
             this.txtTotal.Location = new System.Drawing.Point(658, 529);
             this.txtTotal.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
@@ -119,6 +132,7 @@
             // 
             // txtSubTotal
             // 
+            this.txtSubTotal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.txtSubTotal.Enabled = false;
             this.txtSubTotal.Location = new System.Drawing.Point(658, 498);
             this.txtSubTotal.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
@@ -128,13 +142,17 @@
             // 
             // btnAgregar
             // 
+            this.btnAgregar.AutoSize = true;
+            this.btnAgregar.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAgregar.Font = new System.Drawing.Font("Nirmala UI Semilight", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnAgregar.Location = new System.Drawing.Point(590, 231);
             this.btnAgregar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(88, 27);
+            this.btnAgregar.Size = new System.Drawing.Size(88, 31);
             this.btnAgregar.TabIndex = 46;
             this.btnAgregar.Text = "Agregar";
-            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.UseVisualStyleBackColor = false;
             this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // txtCantidad
@@ -159,6 +177,9 @@
             // 
             this.dgvDetalle.AllowUserToAddRows = false;
             this.dgvDetalle.AllowUserToDeleteRows = false;
+            this.dgvDetalle.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvDetalle.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvDetalle.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ColId,
@@ -212,10 +233,11 @@
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label7.Location = new System.Drawing.Point(28, 168);
             this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(76, 15);
+            this.label7.Size = new System.Drawing.Size(98, 16);
             this.label7.TabIndex = 42;
             this.label7.Text = "Descuento %";
             // 
@@ -230,20 +252,22 @@
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label6.Location = new System.Drawing.Point(28, 137);
             this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(56, 15);
+            this.label6.Size = new System.Drawing.Size(69, 16);
             this.label6.TabIndex = 40;
             this.label6.Text = "AutoPlan";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label4.Location = new System.Drawing.Point(274, 18);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.Size = new System.Drawing.Size(50, 16);
             this.label4.TabIndex = 35;
             this.label4.Text = "Fecha";
             // 
@@ -258,32 +282,38 @@
             // 
             // btnNCliente
             // 
+            this.btnNCliente.AutoSize = true;
+            this.btnNCliente.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnNCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNCliente.Font = new System.Drawing.Font("Nirmala UI Semilight", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnNCliente.Location = new System.Drawing.Point(399, 72);
             this.btnNCliente.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnNCliente.Name = "btnNCliente";
-            this.btnNCliente.Size = new System.Drawing.Size(174, 27);
+            this.btnNCliente.Size = new System.Drawing.Size(174, 31);
             this.btnNCliente.TabIndex = 33;
             this.btnNCliente.Text = "Nuevo Cliente";
-            this.btnNCliente.UseVisualStyleBackColor = true;
+            this.btnNCliente.UseVisualStyleBackColor = false;
             this.btnNCliente.Click += new System.EventHandler(this.btnNCliente_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label3.Location = new System.Drawing.Point(28, 106);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(57, 15);
+            this.label3.Size = new System.Drawing.Size(75, 16);
             this.label3.TabIndex = 32;
             this.label3.Text = "Vendedor";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label2.Location = new System.Drawing.Point(28, 77);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 15);
+            this.label2.Size = new System.Drawing.Size(55, 16);
             this.label2.TabIndex = 31;
             this.label2.Text = "Cliente";
             // 
@@ -308,11 +338,11 @@
             // lblNroFactura
             // 
             this.lblNroFactura.AutoSize = true;
-            this.lblNroFactura.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNroFactura.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.lblNroFactura.Location = new System.Drawing.Point(14, 16);
             this.lblNroFactura.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNroFactura.Name = "lblNroFactura";
-            this.lblNroFactura.Size = new System.Drawing.Size(91, 17);
+            this.lblNroFactura.Size = new System.Drawing.Size(96, 16);
             this.lblNroFactura.TabIndex = 28;
             this.lblNroFactura.Text = "Nro. Factura:";
             // 
@@ -329,32 +359,38 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.Location = new System.Drawing.Point(28, 208);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 15);
+            this.label1.Size = new System.Drawing.Size(50, 16);
             this.label1.TabIndex = 54;
             this.label1.Text = "Marca";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // btnCargar
             // 
+            this.btnCargar.AutoSize = true;
+            this.btnCargar.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnCargar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCargar.Font = new System.Drawing.Font("Nirmala UI Semilight", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnCargar.Location = new System.Drawing.Point(399, 196);
             this.btnCargar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnCargar.Name = "btnCargar";
-            this.btnCargar.Size = new System.Drawing.Size(88, 27);
+            this.btnCargar.Size = new System.Drawing.Size(88, 31);
             this.btnCargar.TabIndex = 55;
             this.btnCargar.Text = "Cargar";
-            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.UseVisualStyleBackColor = false;
             this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label5.Location = new System.Drawing.Point(28, 242);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(56, 15);
+            this.label5.Size = new System.Drawing.Size(69, 16);
             this.label5.TabIndex = 54;
             this.label5.Text = "Producto";
             this.label5.Click += new System.EventHandler(this.label1_Click);
@@ -362,35 +398,44 @@
             // rbPlan1
             // 
             this.rbPlan1.AutoSize = true;
-            this.rbPlan1.Location = new System.Drawing.Point(142, 132);
+            this.rbPlan1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.rbPlan1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rbPlan1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.rbPlan1.Location = new System.Drawing.Point(142, 136);
             this.rbPlan1.Name = "rbPlan1";
-            this.rbPlan1.Size = new System.Drawing.Size(148, 19);
+            this.rbPlan1.Size = new System.Drawing.Size(159, 17);
             this.rbPlan1.TabIndex = 56;
             this.rbPlan1.TabStop = true;
             this.rbPlan1.Text = "36 Cuotas - 23% interes";
-            this.rbPlan1.UseVisualStyleBackColor = true;
+            this.rbPlan1.UseVisualStyleBackColor = false;
             // 
             // rbPlan2
             // 
             this.rbPlan2.AutoSize = true;
-            this.rbPlan2.Location = new System.Drawing.Point(296, 131);
+            this.rbPlan2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.rbPlan2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rbPlan2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.rbPlan2.Location = new System.Drawing.Point(301, 136);
             this.rbPlan2.Name = "rbPlan2";
-            this.rbPlan2.Size = new System.Drawing.Size(148, 19);
+            this.rbPlan2.Size = new System.Drawing.Size(160, 17);
             this.rbPlan2.TabIndex = 56;
             this.rbPlan2.TabStop = true;
             this.rbPlan2.Text = "84 Cuotas - 30% Interes";
-            this.rbPlan2.UseVisualStyleBackColor = true;
+            this.rbPlan2.UseVisualStyleBackColor = false;
             // 
             // rbPlan3
             // 
             this.rbPlan3.AutoSize = true;
-            this.rbPlan3.Location = new System.Drawing.Point(450, 131);
+            this.rbPlan3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.rbPlan3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rbPlan3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.rbPlan3.Location = new System.Drawing.Point(461, 136);
             this.rbPlan3.Name = "rbPlan3";
-            this.rbPlan3.Size = new System.Drawing.Size(156, 19);
+            this.rbPlan3.Size = new System.Drawing.Size(167, 17);
             this.rbPlan3.TabIndex = 56;
             this.rbPlan3.TabStop = true;
             this.rbPlan3.Text = "120 Cuotas - 34% Cuotas";
-            this.rbPlan3.UseVisualStyleBackColor = true;
+            this.rbPlan3.UseVisualStyleBackColor = false;
             // 
             // FrmAltaFactura
             // 
@@ -425,6 +470,7 @@
             this.Controls.Add(this.cboCliente);
             this.Controls.Add(this.cboVendedor);
             this.Controls.Add(this.lblNroFactura);
+            this.Font = new System.Drawing.Font("Nirmala UI Semilight", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "FrmAltaFactura";
